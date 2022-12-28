dc580788d2eb86a9cb964bc80d74a35c925cee48
Adrian Nads <marcelonads@gmail.com>
2022-12-28 22:16:11 +0800

    DONE for the validation of the forms
    DONE for changing the button into checkbox for showing the modal

---
+++ Modal/Javascript/index.js
@@ -0,0 +1,95 @@
+
+let modal = document.querySelector("#modal");
+let modalBg = document.querySelector('#modal-bg');
+let emailField = document.querySelector('input[type="email"]');
+let passwordField = document.querySelector('input[type="password"]');
+let btnLogin = document.querySelector('#btn-login');
+
+modal.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
+
+modal.addEventListener('submit', (event) => {
+
+    let emailValidFormat = /\w+(@gmail.com|@yahoo.com)/;
+
+    let passwordCharactersValid = /[A-Za-z0-9_]/;
+    let passwordValidFormat = /[\w+]{8,}/;
+
+    if (!String(emailField.value).match(emailValidFormat)) {
+        emailField.parentElement.parentElement.children[1].style.display = 'block';
+    }
+
+    if (!String(passwordField.value).match(passwordValidFormat) || !String(passwordField.value).match(passwordCharactersValid)) {
+        passwordField.parentElement.parentElement.children[1].style.display = 'block';
+        passwordField.parentElement.parentElement.children[1].textContent = 'Password must be at least 8 characters long. Valid characters are alphanumeric such as letters, numbers and (_).';
+    }
+}) ;
+
+btnLogin.addEventListener('click', (e) => {
+
+    let emailValidFormat = /\w+(@gmail.com|@yahoo.com)/;
+
+    let passwordCharactersValid = /\W+/;
+    let passwordValidFormat = /[\w+]{8,}/;
+
+    if (!String(emailField.value).match(emailValidFormat)) {
+        emailField.parentElement.parentElement.children[1].style.display = 'block';
+    }
+
+    if (!String(passwordField.value).match(passwordValidFormat) || String(passwordField.value).match(passwordCharactersValid)) {
+        passwordField.parentElement.parentElement.children[1].style.display = 'block';
+        passwordField.parentElement.parentElement.children[1].textContent = 'Password must be at least 8 characters long. Valid characters are alphanumeric such as letters, numbers and (_).';
+        console.log("PASSWORD IS NOT VALID")
+    }
+});
+
+modalBg.addEventListener('click', (e) => {
+
+    document.querySelector('#checkbox1').checked = false;
+    document.querySelector('input[type="password"]').value = '';
+});
+
+document.querySelectorAll('.input-field').forEach(input => {
+
+    console.log('WOW')
+    input.addEventListener('blur', (e) => {
+
+        if (e.currentTarget.value === '') {
+            input.classList.add('on-blur');
+        }
+        else {
+            input.classList.remove('on-blur');
+        }
+    });
+});
+
+emailField.addEventListener('input', (e) => {
+
+    emailField.parentElement.parentElement.children[1].style.display = 'none';
+});
+
+passwordField.addEventListener('input', (e) => {
+
+    passwordField.parentElement.parentElement.children[1].style.display = 'none';
+});
+
+function CheckFormsValidity() {
+let emailValidFormat = /\w+(@gmail.com|@yahoo.com)/;
+
+    let passwordCharactersValid = /[\w+]/;
+    let passwordValidFormat = /[\w+]{8,}/;
+
+    if (!String(emailField.value).match(emailValidFormat)) {
+        emailField.parentElement.parentElement.children[1].style.display = 'block';
+        return false;
+    }
+
+    if (!String(passwordField.value).match(passwordValidFormat) || !String(passwordField.value).match(passwordCharactersValid)) {
+        passwordField.parentElement.parentElement.children[1].style.display = 'block';
+        passwordField.parentElement.parentElement.children[1].textContent = 'Password must be at least 8 characters long. Valid characters are alphanumeric such as letters, numbers and (_).';
+        return false;
+    }
+
+    return true;
+}
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # CSS-PRACTICES
 A repository that contains all the projects that used CSS. It includes the recap of the fundamentals of CSS.
+
+# Flexbox
+
+## What I learned?
+- Flexbox can be used to align elements horizontally or vertically.
+- Some of the properties of flexbox are:
+  - `justify-content`
+  will align the items into its main axis. 
+  - `align-items` will align the items in cross axis.
+  - `align-self` aligns the specified flex-item.
+
+
+# Grid
+
+## What I learned?
+
+- Grid is a layout that positions elements in a horizontal or vertical axis.
+- Grid has a grid-specified unit `fr` that stands for a fraction of each column.
+- To make an efficient grid layout, you can use `grid-template area` that positions  the element to a specified spots.
+- Example: 
+
+```
+grid-template-area: 
+'header header header header'
+'section1 section1 section1 sidebar'
+'footer' 'footer' 'footer' 'footer'
+```
+
+then you can assign an area to each element
+
+```
+header {
+    grid-area: header; 
+}
+```
+
+NOTE: If you are assigning the area, it is not in a STRING.
+
